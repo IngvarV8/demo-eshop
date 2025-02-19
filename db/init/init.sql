797d23286af746869eeb3df5be79ff69ff6e04f1
Ihor Melika <ihor.melika@student.tuke.sk>
2025-02-19 23:54:03 +0100

    updated sql init

--- db/init/init.sql
+++ db/init/init.sql
@@ -1,9 +1,24 @@
 CREATE TABLE items (
-    name VARCHAR(50),
-    pieces INT
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    quantity INT
 );
 
-INSERT INTO items (name, pieces) VALUES
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE order_items (
+    order_id INT NOT NULL,
+    item_id INT NOT NULL,
+    quantity INT NOT NULL,
+    PRIMARY KEY (order_id, item_id),
+    FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE,
+    FOREIGN KEY (item_id) REFERENCES items(id) ON DELETE CASCADE
+);
+
+INSERT INTO items (name, quantity) VALUES
 ('Nohavice', 7),
 ('Tričko', 10),
 ('Mikina', 3),
